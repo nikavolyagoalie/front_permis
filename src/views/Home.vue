72ee7cf417cea014b2ed518a58bfc2a059b571b2
applicant14 <applicant14@clout.one>
2022-09-24 11:52:09 +0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <button @click="createPermission">create</button>
+
+</template>
+
+<script>
+    import API from "@/api/api";
+
+    export default {
+        name: 'Home',
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+            getTemplates() {
+                API.getTemplates()
+            },
+            createPermission() {
+                API.createPermission()
+            }
+        },
+        mounted() {
+            this.getTemplates()
+        }
+    }
+</script>
